4b156341aa6e2c68c54b6d0367a2567695840546
NihadFit <nihadfit@gmail.com>
2021-05-25 18:01:52 +0200

    BaseURL

--- MirzaTest/Startup.cs
+++ MirzaTest/Startup.cs
@@ -34,6 +34,8 @@ namespace MirzaTest
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseForwardedHeaders();
+            app.UsePathBase("/mirzav3");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
